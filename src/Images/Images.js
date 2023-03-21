e4a8a616639bc53a5952c3612b575f1c6c005f70
Safinpeal222 <pealmhmd222@gmail.com>
2023-03-22 00:47:19 +0600

    delete button added

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 
 
 const Images = () => {
+  const [files,setFiles]=useState([]);
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:8000/getname').then(res=>{
+    setFiles(res.data);
+  })
+  },[])
     
+        const handleDelete=(deletedfile)=>{
+          console.log(deletedfile);
+          axios.post('http://127.0.0.1:8000/deletedfile',{name:deletedfile}).then(res=>{
+            if(res.data){
+              const newfiles = files.filter(file=> file.filename !== deletedfile);
+                setFiles(newfiles)
         
-            const handleDownload=()=>{
+            }
+          })
+        }
+            const handleDownload=(selectedfile)=>{
+              
                 
-                axios.get('http://127.0.0.1:8000/download/2ViHW3b4v4DHaDzDTK0hhCID8S0ur7Mocr54N04G.jpg',{
+                axios.get(`http://127.0.0.1:8000/download/${selectedfile}`,{
                     responseType: 'blob',
                   }).then((response) => {
                     const url = window.URL.createObjectURL(new Blob([response.data]));
                     const link = document.createElement('a');
                     link.href = url;
-                    link.setAttribute('download','2ViHW3b4v4DHaDzDTK0hhCID8S0ur7Mocr54N04G.jpg' );
+                    link.setAttribute('download',selectedfile);
                     document.body.appendChild(link);
                     link.click();
                   }).catch((error) => {
@@ -26,7 +42,19 @@ const Images = () => {
     
     return (
         <div>
-            <button onClick={handleDownload}>Download</button>
+        {
+          files.map((file)=>
+       <div>
+          <p>{file.filename}</p>
+          <button className="btn btn-primary m-5" onClick={()=>handleDownload(file.filename)}>Download</button>
+          <button className="btn btn-primary" onClick={()=>handleDelete(file.filename)}>Delete</button>
+          </div>
+
+          )
+
+        }
+
+            
 
         </div>
     );
